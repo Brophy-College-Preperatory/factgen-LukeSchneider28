c7d810d0ada13d16912e5c8ebb6685fc73597569
LukeSchneider28 <lschneider28@brophybroncos.org>
2024-10-25 02:10:10 +0000

    your comment

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var factList = [
- "Fact 1",
+ "Fact 1", /*labeling all of the facts from 1-4*/
  "Fact 2", 
  "Fact 3", 
  "Fact 4"];
 
-var fact = document.getElementById("fact");
+var fact = document.getElementById("fact"); /*these are all variables that will be used in the code below*/
 var myBtn = document.getElementById("myBtn");
 var count = 0;
 
-myBtn.addEventListener("click", displayFact);
+myBtn.addEventListener("click", displayFact); 
 
 function displayFact(){
-  fact.innerHTML = factList[count];
+  fact.innerHTML = factList[count];/*defining the funciton of display fact*/
   count++;
   if (count == factList.length){
     count = 0;
